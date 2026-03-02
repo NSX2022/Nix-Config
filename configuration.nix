deff4ad4372c26f088011b4c7aef0c369192faa5
Ned Stark <nedstarkxy@gmail.com>
2026-03-02 00:36:35 +0000

    Bluetooth

--- configuration.nix
+++ configuration.nix
@@ -115,12 +115,13 @@
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
+    # System
+    blueman # For bluetooth
     # Utilities
     wget
     git
     alacritty
     zoxide
-    gedit
     fastfetch
     ffmpeg
     yt-dlp
