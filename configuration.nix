c20615c769b01af30a46c58d5225daa3e1637f73
NSX2022 <nedstarkxy@gmail.com>
2026-03-17 16:57:51 -0400

    Use GRUB bootloader

--- configuration.nix
+++ configuration.nix
@@ -71,8 +71,15 @@
     ];
 
   # Use the systemd-boot EFI boot loader
-  boot.loader.systemd-boot.enable = true;
+  # boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  # Use GRUB, autodetect other bootable media
+  boot.loader.grub = {
+    enable = true;
+    useOSProber = true;
+    efiSupport = true;
+    device = "nodev";
+  };
 
   networking.hostName = "grimoire";
 
@@ -131,7 +138,6 @@
       
       # Games
       rogue
-      vintagestory
       nethack
       # Utilities
       ollama-vulkan
@@ -191,11 +197,11 @@
     chromium
     python315
     cmake
+    gnumake
     gcc
     typescript
     # xfce
     xfce.xfce4-cpugraph-plugin
-    xfce.xfce4-battery-plugin
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -221,23 +227,8 @@
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
   # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
+  
+  # Do NOT change this value 
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "25.11"; # Did you read the comment?
 
